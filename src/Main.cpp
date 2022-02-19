4db569b2474a14d355d6c070732c1a9e60a5689e
Callum 🍺 <callumimacmillan@gmail.com>
2022-02-19 21:13:50 +0000

    Added SDL 2 with hello world program

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,8 +2,10 @@
 
 #include "vector/Vector.h"
 #include "c/cprogramming.h"
+#include "window/Window.h"
 
 int main() {
     c2_main();
+    helloWorldWindow();
 }
 
---
+++ src/window/Window.cpp
@@ -0,0 +1,36 @@
+#include <SDL.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+void helloWorldWindow() {
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+    }
+    window = SDL_CreateWindow(
+                    "hello_sdl2",
+                    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                    SCREEN_WIDTH, SCREEN_HEIGHT,
+                    SDL_WINDOW_SHOWN
+                    );
+    if (window == NULL) {
+        fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+    }
+    screenSurface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(window);
+    SDL_Event e;
+    bool quit = false;
+    while (!quit){
+        while (SDL_PollEvent(&e)){
+            if (e.type == SDL_QUIT){
+                quit = true;
+            }
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
