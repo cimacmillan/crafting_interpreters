3cdf541a5055d92ca2cde2f1d5c88e36482d81fe
Callum Macmillan <callumimacmillan@gmail.com>
2022-11-19 17:22:13 +0000

    Added basic constnat

--- src/clox/vm.c
+++ src/clox/vm.c
@@ -1,6 +1,7 @@
 #include "vm.h"
 #include "chunk.h"
 #include "debug.h"
+#include "value.h"
 
 #define DEBUG_PRINT
 
@@ -15,6 +16,7 @@ void lox_vm_free() {
 
 lox_vm_result lox_vm_run(lox_chunk *chunk) {
 #define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (chunk->constants.code[READ_BYTE()])
     vm.ip = chunk->bytecode.code;
     for (;;) {
         #ifdef DEBUG_PRINT
@@ -22,12 +24,19 @@ lox_vm_result lox_vm_run(lox_chunk *chunk) {
         #endif
         uint8_t instruction = READ_BYTE();
         switch (instruction) {
-            case OP_RETURN:
+            case OP_RETURN: {
                 return LOX_VM_SUCCESS;
+            }
+            case OP_CONSTANT: {
+                lox_value val = READ_CONSTANT();
+                lox_value_print(val);
+                printf("\n");
+                break;
+            }
         }
     }
-#undef READ_BYTE
-
     return LOX_VM_SUCCESS;
+#undef READ_BYTE
+#undef READ_CONSTANT
 }
 
